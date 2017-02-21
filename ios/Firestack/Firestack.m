010d568348fedb1345ae3a119a01cce194b1eae3
Matt Jeanes <MattBlooJeans@gmail.com>
2017-02-21 18:52:47 -0500

    Convert to RN 40 syntax <React/Thing.h>

--- ios/Firestack/Firestack.m
+++ ios/Firestack/Firestack.m
@@ -52,12 +52,14 @@ withLaunchOptions: (NSDictionary *) launchOptions
 {
     dispatch_once(&onceToken, ^{
         _sharedInstance = instance;
-        
+
+        /* RCTReloadNotification is deprecated
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(reloadFirestack)
                                                      name:RCTReloadNotification
                                                    object:nil];
-        
+        */
+
         [[NSNotificationCenter defaultCenter]
          postNotificationName:kFirestackInitialized
          object:nil];
